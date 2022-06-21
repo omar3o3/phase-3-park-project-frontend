2bfe8031a6d77ba68bb31eadcd65ef07f992faf0
omar3o3 <otantawy53@gmail.com>
2022-06-21 13:11:30 -0400

    forgot i was in main

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -13,21 +13,35 @@ function HomePage() {
       .then(data => setEvents(data.slice(0, 20)))
   }, [])
 
+
+  let dateConverter = (data) =>{
+    let d = new Date(data)
+    console.log(d.toString());
+  }
+
   return (
     <div className="m-3">
       {events.map(event => {
         return (
-          <Card className="m-3" style={{ width: '18rem' }}>
+          <Card key = {event.event_id} className="m-3" style={{ width: '18rem' }}>
             <Card.Body>
               <Card.Title className="fs-3">{event.event_name}</Card.Title>
               <Card.Text>
-                <span className="fw-bold">Location:</span> {event.event_location}
+                <span className="fw-bold">Location:</span>
+                <span className="mx-2">{event.event_location}</span>
               </Card.Text>
             </Card.Body>
             <ListGroup className="list-group-flush">
+
+              <ListGroup.Item>
+                <span className="fw-bold">Borough:</span>
+                <span className="mx-2">{event.event_borough}</span>
+              </ListGroup.Item>
+
               <ListGroup.Item>
                 <span className="fw-bold">Event Type:</span>
-                {event.event_type}
+                <span className="mx-2">{event.event_type}</span>
+                {dateConverter(event.start_date_time)}
               </ListGroup.Item>
             </ListGroup>
           </Card>
