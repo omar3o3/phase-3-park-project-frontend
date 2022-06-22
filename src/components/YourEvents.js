2e629c9d835f6bcf9b6a7f2adc795d2468df1b09
omar3o3 <otantawy53@gmail.com>
2022-06-22 18:17:38 -0400

    i am sad

--- src/components/YourEvents.js
+++ src/components/YourEvents.js
@@ -14,8 +14,9 @@ function YourEvents() {
   useEffect(() => {
     fetch('http://localhost:9292/your-events')
       .then(resp => resp.json())
-      .then(data => setEventsData(data))
+      // .then(data => setEventsData(data))
     // .then(data => console.log(data))
+    .then(data => console.log(timeConverter(data[0].your_events[0].start_date_time)))
   }, []) 
 
   // Thie function transfer date to the format we want
@@ -36,58 +37,65 @@ function YourEvents() {
     let ampmFormat = hoursInTwelve + ":" + minutes + " " + AMOrPM
     return (ampmFormat)
   }
+  // console.log(eventsData)
+  // console.log("above is eventsData")
+  // console.log(eventsData[0].borough_name)
+  // console.log("this level contains borough_name, event_types and your_events object")
+  //console.log(eventsData[0].your_events)
+
 
   return (
-    <div>
-      <Row xs={1} md={2} lg={4} className="justify-content-center">
-        {eventsData.map(event => {
-          return (
-            <Col className="m-3" key={event.your_events.id}>
-              <Card style={{ width: '18rem' }} bg="light">
+    <div></div>
+    // <div>
+    //   <Row xs={1} md={2} lg={4} className="justify-content-center">
+    //     {eventsData.map(event => {
+    //       return (
+    //         <Col className="m-3" key={event.your_events.id}>
+    //           <Card style={{ width: '18rem' }} bg="light">
 
-                <Card.Body>
-                  <Card.Title className="fs-3">{event.your_events.event_name}</Card.Title>
-                  <Card.Text>
-                    <span className="fw-bold">Location:</span>
-                    <span className="mx-2">{event.your_events.event_location}</span>
-                  </Card.Text>
-                </Card.Body>
+    //             <Card.Body>
+    //               <Card.Title className="fs-3">{event.your_events.event_name}</Card.Title>
+    //               <Card.Text>
+    //                 <span className="fw-bold">Location:</span>
+    //                 <span className="mx-2">{event.your_events.event_location}</span>
+    //               </Card.Text>
+    //             </Card.Body>
 
-                <ListGroup className="list-group-flush">
+    //             <ListGroup className="list-group-flush">
 
-                  <ListGroup.Item>
-                    <span className="fw-bold">Borough:</span>
-                    <span className="mx-2">{event.borough_name}</span>
-                  </ListGroup.Item>
+    //               <ListGroup.Item>
+    //                 <span className="fw-bold">Borough:</span>
+    //                 <span className="mx-2">{event.borough_name}</span>
+    //               </ListGroup.Item>
 
-                  {/* <ListGroup.Item>
-                    <span className="fw-bold">Start Date:</span>
-                    <span className="mx-2">{dateConverter(event.your_events.start_date_time)}</span>
-                  </ListGroup.Item>
+    //               {/* <ListGroup.Item>
+    //                 <span className="fw-bold">Start Date:</span>
+    //                 <span className="mx-2">{dateConverter(event.your_events.start_date_time)}</span>
+    //               </ListGroup.Item>
 
-                  <ListGroup.Item>
-                    <span className="fw-bold">Start Time:</span>
-                    <span className="mx-2">{timeConverter(event.your_events.start_date_time)}</span>
-                  </ListGroup.Item>
+    //               <ListGroup.Item>
+    //                 <span className="fw-bold">Start Time:</span>
+    //                 <span className="mx-2">{timeConverter(event.your_events.start_date_time)}</span>
+    //               </ListGroup.Item>
 
-                  <ListGroup.Item>
-                    <span className="fw-bold">End Time:</span>
-                    <span className="mx-2">{timeConverter(event.your_events.end_date_time)}</span>
-                  </ListGroup.Item> */}
+    //               <ListGroup.Item>
+    //                 <span className="fw-bold">End Time:</span>
+    //                 <span className="mx-2">{timeConverter(event.your_events.end_date_time)}</span>
+    //               </ListGroup.Item> */}
 
-                  <ListGroup.Item>
-                    <span className="fw-bold">Event Type:</span>
-                    <span className="mx-2">{event.event_types.event_type_name}</span>
-                  </ListGroup.Item>
+    //               <ListGroup.Item>
+    //                 <span className="fw-bold">Event Type:</span>
+    //                 <span className="mx-2">{event.event_types.event_type_name}</span>
+    //               </ListGroup.Item>
 
-                </ListGroup>
+    //             </ListGroup>
 
-              </Card>
-            </Col>
-          )
-        })}
-      </Row>
-    </div>
+    //           </Card>
+    //         </Col>
+    //       )
+    //     })}
+    //   </Row>
+    // </div>
   )
 }
 
